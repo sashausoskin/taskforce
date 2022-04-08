24f355050d892ba1f7e1e553980a970d381edd0a
Sasha Usoskin <sashausoskin@live.com>
2022-04-08 13:22:03 +0300

    FIX Windows taskbar icon

--- src/index.py
+++ src/index.py
@@ -1,6 +1,12 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from ui.login_form import loginWindow
+import platform
+import ctypes
+
+if platform.system()=="Windows": #This is required for the taskbar icon to work
+    myappid = u'sonicsasha.taskforce.build.version1'
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
 
 app = QApplication(sys.argv)
 
